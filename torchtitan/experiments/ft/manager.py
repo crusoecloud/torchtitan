2a90894eec8c11469516459bef79ec44914e78a7
Tristan Rice <rice@fn.lc>
2026-04-03 13:08:11 -0700

    ft: add support for MCCL and docs updates

--- torchtitan/experiments/ft/manager.py
+++ torchtitan/experiments/ft/manager.py
@@ -11,11 +11,11 @@ from collections.abc import Callable
 from contextlib import AbstractContextManager, nullcontext
 from dataclasses import dataclass
 from datetime import timedelta
+from gc import enable
 from typing import cast, TYPE_CHECKING
 
 import torch
 import torch.distributed as dist
-
 import torch.nn as nn
 from torch.distributed._composable.fsdp.fully_shard import FSDPModule
 from torch.distributed.distributed_c10d import ReduceOp
@@ -93,6 +93,18 @@ class FTManager(Configurable):
             pg = ft.ProcessGroupGloo(timeout=process_group_timeout)
         elif config.process_group == "nccl":
             pg = ft.ProcessGroupNCCL(timeout=process_group_timeout)
+        elif config.process_group == "mccl":
+            import torchcomms
+            from torchft.torchcomms import ProcessGroupTorchComms
+
+            comm = torchcomms.new_comm(
+                "mccl",
+                device=torch.device("cuda"),
+                name="mccl_ft",
+                timeout=process_group_timeout,
+                enable_reconfigure=True,
+            )
+            pg = ProcessGroupTorchComms(comm, timeout=process_group_timeout)
         else:
             raise ValueError(f"Unsupported process group: {config.process_group}")
 
